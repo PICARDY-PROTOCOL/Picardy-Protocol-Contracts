7453b7b0cb2293553b815ce85986eb3859a0844c
1da42 <12879533+blok-hamster@users.noreply.github.com>
2023-01-05 10:23:08 -0800

    new commit

--- contracts/Factory/NftRoyaltySaleFactory.sol
+++ contracts/Factory/NftRoyaltySaleFactory.sol
@@ -111,8 +111,25 @@ contract NftRoyaltySaleFactory is Context {
 }
 
 interface INftRoyaltySaleFactory {
+
+    struct Details {
+        uint maxSupply; 
+        uint maxMintAmount; 
+        uint cost; 
+        uint percentage;
+        string name;
+        string symbol; 
+        string initBaseURI;
+        string artisteName;
+    }
+
+    function createNftRoyalty(Details memory details) external returns(address);
+    
     function getRoyaltyDetails() external view returns (address, uint);
+    
     function updateRoyaltyDetails(uint _royaltyPercentage) external ;
+    
     function getRoyaltyUri(address _royaltyAddress) external view returns (string memory);
+    
     function getLinkToken() external view returns(address);    
 }
--- contracts/Factory/TokenRoyaltySaleFactory.sol
+++ contracts/Factory/TokenRoyaltySaleFactory.sol
@@ -95,6 +95,7 @@ contract TokenRoyaltySaleFactory is Context {
 }
 
 interface ITokenRoyaltySaleFactory{
+    function createTokenRoyalty(uint _askAmount, uint _returnPercentage, string memory creatorName, string memory name) external returns(address);
     function getRoyaltyDetails() external view returns (address, uint);
     function getHubAddress() external view returns (address);
     function getLinkToken() external view returns(address);
